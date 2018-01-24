5ac20311a0ebca861ed61afd757df98a8a7a9f97
dswanson8 <d.swan0204@gmail.com>
2018-01-24 09:44:17 -0700

    gallery practice - Need to finish

---
+++ Week 03/image-gallery/main.js
@@ -0,0 +1,14 @@
+/*
+
+Create a click event listener on the .image-links element.  The listener function
+should set the src attribute of the img.target-image element and prevent the default
+behaviour from occurring.
+
+*/
+
+var targetImage = document.querySelector('img.target-image')
+
+function showImage(evt) {
+    var imageLinks = document.querySelector('.image-links')
+    imageLinks.src = 
+}
